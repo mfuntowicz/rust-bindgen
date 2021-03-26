92c724c2cdbf73a3df46c728770e28edd61447fd
Adrian Taylor <adetaylor@chromium.org>
2021-03-26 15:16:47 -0700

    Denote const receivers on virtual functions.

--- src/ir/function.rs
+++ src/ir/function.rs
@@ -479,7 +479,7 @@ impl FunctionSig {
                     Item::builtin_type(TypeKind::Pointer(class), false, ctx);
                 args.insert(0, (Some("this".into()), ptr));
             } else if is_virtual {
-                let void = Item::builtin_type(TypeKind::Void, false, ctx);
+                let void = Item::builtin_type(TypeKind::Void, is_const, ctx);
                 let ptr =
                     Item::builtin_type(TypeKind::Pointer(void), false, ctx);
                 args.insert(0, (Some("this".into()), ptr));
